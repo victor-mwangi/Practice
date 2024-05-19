6660403b019c5b6d752ee39baaa0667b36b95a6b
victor-mwangi <rigelvicky@gmail.com>
2024-05-19 18:36:37 +0300

    Fresh Start

---
+++ TwoSum.cs
@@ -0,0 +1,18 @@
+﻿//The Below solution is using Brute-Force its not very efficient fot large arrays
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[0];
+    }
+}
